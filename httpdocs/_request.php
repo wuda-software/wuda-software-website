51093b0f46ac9461385a5856b89ac3acc633e609
Daniel Wurzer <wurzer.da@gmail.com>
2023-12-13 16:46:41 +0100

    build: script safety and spam protection

--- httpdocs/_request.php
+++ httpdocs/_request.php
@@ -1,15 +1,19 @@
 <?php
   $now = date("Y-m-d H:i:s");
   $ip = $_SERVER['REMOTE_ADDR'];
-  $senderName = $_POST["request_sender_name"];
-  $senderMail = $_POST["request_sender_mail"];
-  $message = $_POST["request_message"];
+  $senderName = isset($_POST["request_sender_name"]) ? $_POST["request_sender_name"] : null;
+  $senderMail = isset($_POST["request_sender_mail"]) ? $_POST["request_sender_mail"] : null;
+  $message = isset($_POST["request_message"]) ? $_POST["request_message"] : null;
 
+	// Check inputs
+	if (is_null($senderName) || strlen(trim($senderName)) === 0) die("Sender Name not set or empty!");
+	if (is_null($senderMail) || strlen(trim($senderMail)) === 0) die("Sender Mail not set or empty!");
+	if (is_null($message) || strlen(trim($message)) === 0) die("Message not set or empty!");
 
 	// TODO: Create Temp file which stores the IP and Timestamp
 	$logLine = $now . "|" . $ip;
 	file_put_contents("log.txt", $logLine);
-	
+
 	echo "LEL";
 	exit();
 
