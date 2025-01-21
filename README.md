1380710dd11e0ea6fd33f8a0f110c6f28c1cb6d1
Daniel <wuda@MBookP.local>
2025-01-21 13:00:45 +0100

    refactor: only clean website

--- README.md
+++
@@ -1,9 +0,0 @@
-# Getting started
-
-1. run `npm install`
-2. run `npm run dev`
-
-## TODO
-
-- [ ] Host Website on GitHub
-- [ ] Use handlebars.js instead of PUG and Grunt
--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,13 @@
-fetch(
-  "https://raw.githubusercontent.com/wuda-software/module-list/main/apps.json"
-)
+const urlModules = "https://raw.githubusercontent.com/wuda-software/module-list/main/apps.json";
+
+fetch(urlModules)
   .then((resp) => resp.json())
   .then((resp) => {
     let dataLimit;
     const targetEl = document.querySelector(".module-list");
-    if (targetEl.hasAttribute("data-limit"))
-      dataLimit = parseInt(targetEl.getAttribute("data-limit"));
-
+    if (targetEl.hasAttribute("data-limit")) dataLimit = parseInt(targetEl.getAttribute("data-limit"));
     let appList = resp;
     if (dataLimit) appList = resp.slice(0, dataLimit);
-
     appList.map((app) => {
       const el = document.createElement("div");
       el.classList.add("card", "s6", "m3");
@@ -20,15 +17,16 @@ fetch(
         <p style="max-height: 100px; overflow: hidden; text-overflow: ellipsis;
         display: -webkit-box; -webkit-line-clamp: 4;
         -webkit-box-orient: vertical;">${app.description || ""}</p>`;
-
       if (app.name[0] === "✅") el.style.backgroundColor = "#3733";
-
       targetEl.appendChild(el);
     });
   });
 
+// Cubes Animation
+
 setInterval(() => {
   const els = document.querySelectorAll(".cube div span");
+  if (els.length === 0) return;
   const randIndex = Math.ceil(Math.random() * els.length);
   console.log(randIndex);
   els[randIndex].classList.add("active");
